8a2705e776fe9dc339c24d724d26a03428d3945c
ofir ben yamin <ofirbenyamin3@gmail.com>
2019-04-27 00:34:36 +0300

    Initial commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed = 100f;
+    [SerializeField] float thrustSpeed = 100f;
+
+    Rigidbody rb;
+    AudioSource flySound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        flySound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotation();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            case "Finish":
+                print("VICTORY!");
+                break;
+            default:
+                print("DEAD");
+                break;
+        }
+    }
+
+    private void Rotation()
+    {
+        rb.freezeRotation = true;
+        float speed = rotateSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * speed);
+        }
+
+        rb.freezeRotation = false;
+
+
+    }
+
+    private void Thrust()
+    {
+        // float tSpeed = thrustSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustSpeed);
+            if (flySound.isPlaying == false)
+                flySound.Play();
+        }
+        else
+        {
+            flySound.Stop();
+        }
+    }
+}
