9a866364c052acc94c26bdad598742cf55455cde
ofir ben yamin <ofirbenyamin3@gmail.com>
2019-05-10 17:38:33 +0300

    10.5.2019

---
+++ Assets/PlatMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PlatMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 MovementVector;
+    [Range(0, 1)] [SerializeField] float MovementFactor;
+    [SerializeField] Vector3 StartingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = MovementVector * MovementFactor;
+        transform.position = StartingPos + offset;
+    }
+}
